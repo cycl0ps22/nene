b537f6f29a05b9a228d5b1cdadffefeb706c5b02
cycl0ps22 <99908296+cycl0ps22@users.noreply.github.com>
2024-07-01 15:22:35 +0545

    Add files via upload

---
+++ pages/script.js
@@ -0,0 +1,9 @@
+// Add event listener to table of contents links
+document.querySelectorAll('#table-of-contents a').forEach(link => {
+    link.addEventListener('click', event => {
+        event.preventDefault();
+        const targetId = link.getAttribute('href').substring(1);
+        const targetElement = document.getElementById(targetId);
+        targetElement.scrollIntoView({ behavior: 'smooth' });
+    });
+});
