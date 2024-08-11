40da41c1dc3fbe5a3d6febf688103e0f3b716587
cycl0ps22 <99908296+cycl0ps22@users.noreply.github.com>
2024-08-11 16:47:58 +0545

    Delete pages directory

--- pages/script.js
+++
@@ -1,9 +0,0 @@
-// Add event listener to table of contents links
-document.querySelectorAll('#table-of-contents a').forEach(link => {
-    link.addEventListener('click', event => {
-        event.preventDefault();
-        const targetId = link.getAttribute('href').substring(1);
-        const targetElement = document.getElementById(targetId);
-        targetElement.scrollIntoView({ behavior: 'smooth' });
-    });
-});
